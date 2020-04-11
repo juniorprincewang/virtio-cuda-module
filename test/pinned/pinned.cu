f095466bc6d3cf693a21721f800e51225dbc0f67
juniorprincewang <15201615161@163.com>
2020-04-11 13:05:30 +0800

    fix test/pinned

--- test/pinned/pinned.cu
+++ test/pinned/pinned.cu
@@ -1,5 +1,6 @@
-#include <stdio.h>  
-#include <cuda_runtime.h>  
+#include <stdio.h>
+#include <cuda_runtime.h>
+#include <unistd.h>
 
 #define checkCudaErrors(call) { \
   cudaError_t err; \
@@ -10,67 +11,106 @@
 }
 
 /* A very simple kernel function */
- __global__ void kernel(int *d_var) {
+__global__ void kernel(int *g_data, int value)
+{
     int idx = blockIdx.x * blockDim.x + threadIdx.x;
-    d_var[idx] += 10; 
-} 
- 
- int * host_p;  
- int * host_result;  
- int * dev_p;  
+    g_data[idx] = g_data[idx] + value;
+}
+
+int checkResult(int *data, const int n, const int x)
+{
+    for (int i = 0; i < n; i++)
+    {
+        if (data[i] != x)
+        {
+            printf("Error! data[%d] = %d, ref = %d\n", i, data[i], x);
+            return 0;
+        }
+    }
+
+    return 1;
+}
  
 int main(void) {  
-      // int ns = 4;
-      // int ns = 1<<8;
-        int ns = 1<<15;
-      int data_size = ns * sizeof(int);
-      
-      /* Allocate host_p as pinned memory */
-      checkCudaErrors( 
-        cudaHostAlloc((void**)&host_p, data_size, 
-        cudaHostAllocDefault) );  
-      
-      /* Allocate host_result as pinned memory */
-      checkCudaErrors( 
-        cudaHostAlloc((void**)&host_result, data_size, 
-        cudaHostAllocDefault) );  
-      /* Allocate dev_p on the device global memory */
-      checkCudaErrors( 
-        cudaMalloc((void**)&dev_p, data_size) );  
-      
-      /* Initialise host_p*/
-      for (int i=0; i<ns; i++){  
-           host_p[i] = i + 1;  
-      }  
-      
-      /* Transfer data to the device host_p .. dev_p */
-      checkCudaErrors( 
+    // int ns = 1<<8;
+    int ns = 1<<3;
+    int data_size = ns * sizeof(int);
+    int * host_p;
+    int * host_p2;
+    int * host_result;
+    int * dev_p;
+    int value = 10;
+    dim3 block, grid;
+    size_t page_size = 0;
+    int * h_p1;
+    int * h_p2;
+    int * h_p3;
+    int n=1<<10;
+
+    printf("Allocate data size 0x%x\n", n);
+    h_p1 = (int *)malloc(n);
+    printf("host p1 is at %p\n", h_p1);
+    h_p2 = (int *)malloc(n);
+    printf("host p2 is at %p\n", h_p2);
+    h_p3 = (int *)malloc(n);
+    printf("host p3 is at %p\n", h_p3);
+    checkCudaErrors(
+        cudaHostRegister(h_p1, n, cudaHostRegisterDefault) );
+    checkCudaErrors(
+        cudaHostRegister(h_p2, n, cudaHostRegisterDefault) );
+    checkCudaErrors(
+        cudaHostRegister(h_p3, n, cudaHostRegisterDefault) );
+
+    page_size = sysconf(_SC_PAGE_SIZE);
+    printf("Page size is 0x%lx\n", page_size);
+    printf("Allocate data size 0x%x\n", data_size);
+    /* Allocate host_p as pinned memory */
+    checkCudaErrors(
+        cudaHostAlloc((void**)&host_p, data_size, cudaHostAllocDefault) );
+    printf("host p is at %p\n", host_p);
+    checkCudaErrors(
+        cudaHostAlloc((void**)&host_p2, data_size, cudaHostAllocDefault) );
+    printf("host p2 is at %p\n", host_p2);
+    /* Allocate host_result as pinned memory */
+    checkCudaErrors(
+        cudaHostAlloc((void**)&host_result, data_size, cudaHostAllocDefault) );
+    printf("host result is at %p\n", host_result);
+    /* Allocate dev_p on the device global memory */
+    checkCudaErrors(cudaMalloc((void**)&dev_p, data_size) );
+    printf("dev p is at %p\n", dev_p);
+
+    /* Initialise host_p*/
+    memset(host_p, 0, data_size);
+    /* Transfer data to the device host_p .. dev_p */
+    checkCudaErrors(
         cudaMemcpy(dev_p, host_p, data_size, cudaMemcpyHostToDevice) );
       
     /* Now launch the kernel... */
-    dim3 block, grid;
     // set kernel launch configuration
-    block = dim3(32);
+    block = dim3(2);
     grid  = dim3((ns + block.x - 1) / block.x);
-    kernel<<<grid, block>>>(dev_p);  
+    kernel<<<grid, block>>>(dev_p, value);
     checkCudaErrors(cudaGetLastError());
-      
-      /* Copy the result from the device back to the host */
-      checkCudaErrors( 
+    /* Copy the result from the device back to the host */
+    checkCudaErrors(
         cudaMemcpy(host_result, dev_p, data_size, cudaMemcpyDeviceToHost) );
-      
-      printf("Check if no failures, then pass.\n");      
-      /* and print the result */
-      for (int i=0; i<ns; i++){  
-            if (host_result[i] != i+11)
-                printf("Failed result[%d] = %d\n", i, host_result[i]);  
-      }
-      /*
-       * Now free the memory!
-       */
-      checkCudaErrors( cudaFree(dev_p) );  
-      checkCudaErrors( cudaFreeHost(host_p) );  
-      checkCudaErrors( cudaFreeHost(host_result) );  
-      
-      return 0;  
- } 
+
+    printf("Check if no failures, then pass.\n");
+    int bFinalResults = checkResult(host_result, ns, value);
+    printf("result:%s\n", bFinalResults? "PASS" : "FAILED");
+
+    /*
+    * Now free the memory!
+    */
+    checkCudaErrors( cudaFree(dev_p) );
+    checkCudaErrors( cudaFreeHost(host_p) );
+    checkCudaErrors( cudaFreeHost(host_p2) );
+    checkCudaErrors( cudaFreeHost(host_result) );
+    checkCudaErrors( cudaHostUnregister(h_p1) );
+    checkCudaErrors( cudaHostUnregister(h_p2) );
+    checkCudaErrors( cudaHostUnregister(h_p3) );
+    free(h_p1);
+    free(h_p2);
+    free(h_p3);
+    return 0;
+}
